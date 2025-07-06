7414cdf98b92074376427c9fbf3213c410f350df
fqw23 <166891234+fqw23@users.noreply.github.com>
2025-07-07 01:01:44 +0530

    Create fishing_stats.py
    First Commit

---
+++ fishing_stats.py
@@ -0,0 +1,98 @@
+import math
+import pandas as pd
+import streamlit as st
+import datetime
+@st.cache_data
+def load_data(file):
+    data=pd.read_csv(file,low_memory=False)
+    return data
+def main():
+    st.title('ChattyKathy Fishing Stats')
+    data=load_data('FullData.csv')
+    choicereset=st.multiselect('ResetDate:',data['Reset Date'].unique())
+    filtered_data=data
+    if choicereset:
+        filtered_data=filtered_data[filtered_data['Reset Date'].isin(choicereset)]
+    filtered_data['date']=pd.to_datetime(data['date'],errors='coerce',yearfirst=True)
+    filtered_data['DateTime']=pd.to_datetime(filtered_data['DateTime'], dayfirst=True, format="%d/%m/%Y %I:%M:%S %p",errors='coerce').dt.tz_localize("UTC")
+    print(filtered_data[filtered_data['date']=='NaT'])
+    valid_dates=filtered_data['date'].dropna()
+    if not valid_dates.empty:
+        min_dt=valid_dates.min().date()
+        max_dt=valid_dates.max().date()
+    else:
+        min_dt=datetime.date(2024,1,1)
+        max_dt=datetime.date(2034,1,1)
+    start_date=st.date_input("Start Date",min_value=min_dt,max_value=max_dt,value=min_dt)
+    end_date=st.date_input("End Date",min_value=min_dt,max_value=max_dt,value=max_dt)
+    if start_date > end_date:
+        st.error('Error: End date must be greater than start date.')
+    choicefishtype=st.multiselect("Pick Fish Category:",filtered_data['category'].unique())
+    if choicefishtype:
+        filtered_data=filtered_data[filtered_data['category'].isin(choicefishtype)]
+    choicefishrarity=st.multiselect("Pick Fish Rarity:",filtered_data['rarity'].unique())
+    if choicefishrarity:
+        filtered_data=filtered_data[filtered_data['rarity'].isin(choicefishrarity)]
+    max_weight=max(filtered_data['weight'].astype(float))
+    start_weight,end_weight=st.slider('Fish weight:',min_value=0.0,max_value=max_weight,value=(0.0,max_weight))
+    filtered_data=filtered_data[(filtered_data['weight']>=start_weight)&(filtered_data['weight']<=end_weight)]
+
+    max_coins=max(filtered_data['value'].astype(int))
+    start_coins,end_coins=st.slider('Fish Coin:',min_value=1,max_value=max_coins,value=(1,max_coins))
+    filtered_data=filtered_data[(filtered_data['value']>=start_coins)&(filtered_data['value']<=end_coins)]
+    choice0=st.selectbox('GroupBy:',['User','Fish','Catch'])
+
+    filtered_data=filtered_data.rename(columns={'username':'User'})
+    x=filtered_data.reset_index()
+    if choice0=='User':
+        x=filtered_data.groupby(['User']).agg(
+            TotalCatches=('fishname','count'),
+            TotalCoins=('value','sum'),
+            FishesCaught=('fishname',lambda x:x.nunique()),
+            FirstCatches=('isNew',lambda x: (x=='New').sum()),
+            BiggestCatch=('value','max'),
+            BiggestCatchIndex=('value','idxmax'),
+            HeaviestCatch=('weight','max'),
+            HeaviestCatchIndex=('weight','idxmax'),
+        ).reset_index()
+        x=x.sort_values(by='TotalCatches',ascending=False)
+        x['BiggestCatchFish']=""
+        x['HeaviestCatchFish']=""
+        for idx,row in x.iterrows():
+            index1=row['BiggestCatchIndex']
+            x.loc[idx,'BiggestCatchFish']=filtered_data.loc[index1,'fishname']
+            index2=row['HeaviestCatchIndex']
+            x.loc[idx,'HeaviestCatchFish']=filtered_data.loc[index2,'fishname']
+        if st.button('Check Stats'):
+            x=x.drop(columns=['BiggestCatchIndex','HeaviestCatchIndex'])
+            st.dataframe(x)
+    elif choice0=='Fish':
+        x=filtered_data.groupby(['fishname']).agg(
+            TotalCatches=('fishname','count'),
+            BiggestCatch=('value','max'),
+            HeaviestCatch=('weight','max'),
+            UniqueCatchers=('User',lambda x:x.nunique()),
+            FirstCatch=('DateTime','min'),
+            FirstFishCatcherIndex=('DateTime','idxmin'),
+            BiggestFishCatcherIndex=('weight','idxmax'),
+        ).reset_index()
+        x=x.sort_values(by='TotalCatches',ascending=False)
+        x['BiggestFishCatcher']=""
+        x['FirstFishCatcher']=""
+        for idx,row in x.iterrows():
+            index1=row['BiggestFishCatcherIndex']
+            x.loc[idx,'BiggestFishCatcher']=filtered_data.loc[index1,'User']
+            index2=row['FirstFishCatcherIndex']
+            x.loc[idx,'FirstFishCatcher']=filtered_data.loc[index2,'User']
+        x=x.rename(columns={'fishname':'Fish'})
+        x=x.drop(columns=['BiggestFishCatcherIndex','FirstFishCatcherIndex']).reset_index()
+        if st.button('Check Stats'):
+            #x=x.drop(columns=['rating','date','time','Reset Date','index'])
+            st.dataframe(x)
+    elif choice0=='Catch':
+        if st.button('Check Stats'):
+            x=x.drop(columns=['rating','date','time','Reset Date','index']).reset_index()
+            st.dataframe(x)
+
+if __name__=='__main__':
+    main()
